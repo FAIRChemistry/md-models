7c76713d9c67997ee774b5598df6a919bf95d700
Jan Range <range.jan@web.de>
2024-06-03 15:34:49 +0200

    parse schema enum

---
+++ src/json/datatype.rs
@@ -0,0 +1,203 @@
+use std::{fmt::Display, str::FromStr};
+
+#[derive(Debug, PartialEq)]
+pub enum DataType {
+    String,
+    Integer,
+    Number,
+    Boolean,
+    Object { properties: serde_json::Value },
+    Array,
+    Enum { values: Vec<String> },
+    Reference { reference: String },
+}
+
+impl Display for DataType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            DataType::String => write!(f, "string"),
+            DataType::Integer => write!(f, "integer"),
+            DataType::Number => write!(f, "number"),
+            DataType::Boolean => write!(f, "boolean"),
+            DataType::Array => write!(f, "array"),
+            DataType::Reference { reference } => write!(f, "reference [{}]", reference),
+            DataType::Object { properties } => {
+                let properties = properties
+                    .as_object()
+                    .unwrap()
+                    .keys()
+                    .map(|k| k.to_string())
+                    .collect::<Vec<String>>()
+                    .join(", ");
+                write!(f, "object [{}]", properties)
+            }
+            DataType::Enum { values } => {
+                let values = values.join(", ");
+                write!(f, "enum [{}]", values)
+            }
+        }
+    }
+}
+
+impl FromStr for DataType {
+    type Err = String;
+    fn from_str(s: &str) -> Result<Self, Self::Err> {
+        match s {
+            "string" => Ok(DataType::String),
+            "integer" => Ok(DataType::Integer),
+            "number" => Ok(DataType::Number),
+            "boolean" => Ok(DataType::Boolean),
+            "array" => Ok(DataType::Array),
+            _ => Err(format!("Unknown data type: {}", s)),
+        }
+    }
+}
+
+impl DataType {
+    pub fn from_object(value: &serde_json::Value) -> Self {
+        if let Some(reference) = value.get("$ref") {
+            return DataType::Reference {
+                reference: reference
+                    .as_str()
+                    .unwrap()
+                    .split('/')
+                    .last()
+                    .unwrap()
+                    .to_string(),
+            };
+        } else if let Some(values) = value.get("enum") {
+            let values = values
+                .as_array()
+                .unwrap()
+                .iter()
+                .map(|v| v.as_str().unwrap().to_string())
+                .collect();
+            return DataType::Enum { values };
+        } else if let Some(data_type) = value.get("type") {
+            let data_type = data_type.as_str().unwrap();
+            if data_type == "object" {
+                let properties = value.get("properties").unwrap();
+                return DataType::Object {
+                    properties: properties.clone(),
+                };
+            } else {
+                return data_type.parse().expect(
+                    "Could not parse the data type. Make sure the data type is a valid type.",
+                );
+            }
+        } else {
+            panic!("Could not find a data type in the JSON schema");
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_data_type_from_str() {
+        let data_type = "string".parse::<DataType>().unwrap();
+        assert_eq!(data_type, DataType::String);
+    }
+
+    #[test]
+    fn test_data_type_from_object() {
+        let data_type = DataType::from_object(&serde_json::json!({
+            "type": "string"
+        }));
+        assert_eq!(data_type, DataType::String);
+    }
+
+    #[test]
+    fn test_data_type_from_object_with_enum() {
+        let data_type = DataType::from_object(&serde_json::json!({
+            "enum": ["one", "two"]
+        }));
+        assert_eq!(
+            data_type,
+            DataType::Enum {
+                values: vec!["one".to_string(), "two".to_string()]
+            }
+        );
+    }
+
+    #[test]
+    fn test_data_type_from_object_with_reference() {
+        let data_type = DataType::from_object(&serde_json::json!({
+            "$ref": "#/definitions/Person"
+        }));
+        assert_eq!(
+            data_type,
+            DataType::Reference {
+                reference: "Person".to_string()
+            }
+        );
+    }
+
+    #[test]
+    fn test_data_type_from_object_with_object() {
+        let data_type = DataType::from_object(&serde_json::json!({
+            "type": "object",
+            "properties": {
+                "name": {
+                    "type": "string"
+                }
+            }
+        }));
+        assert_eq!(
+            data_type,
+            DataType::Object {
+                properties: serde_json::json!({
+                    "name": {
+                        "type": "string"
+                    }
+                })
+            }
+        );
+    }
+
+    #[test]
+    fn test_data_type_display() {
+        let data_type = DataType::String;
+        assert_eq!(data_type.to_string(), "string");
+
+        let data_type = DataType::Integer;
+        assert_eq!(data_type.to_string(), "integer");
+
+        let data_type = DataType::Number;
+        assert_eq!(data_type.to_string(), "number");
+
+        let data_type = DataType::Boolean;
+        assert_eq!(data_type.to_string(), "boolean");
+
+        let data_type = DataType::Array;
+        assert_eq!(data_type.to_string(), "array");
+
+        let data_type = DataType::Reference {
+            reference: "Person".to_string(),
+        };
+        assert_eq!(data_type.to_string(), "reference [Person]");
+
+        let data_type = DataType::Object {
+            properties: serde_json::json!({
+                "name": {
+                    "type": "string"
+                }
+            }),
+        };
+        assert_eq!(data_type.to_string(), "object [name]");
+
+        let data_type = DataType::Enum {
+            values: vec!["one".to_string(), "two".to_string()],
+        };
+        assert_eq!(data_type.to_string(), "enum [one, two]");
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_display_panic() {
+        let data_type = DataType::from_object(&serde_json::json!({}));
+        data_type.to_string();
+    }
+}
--- src/json/parser.rs
+++ src/json/parser.rs
@@ -1,53 +1,19 @@
-use std::{error::Error, fmt::Display, path::Path, str::FromStr};
-
 use crate::{
     attribute::{AttrOption, Attribute},
     datamodel::DataModel,
     markdown::frontmatter::FrontMatter,
-    object::Object,
+    object::{Enumeration, Object},
 };
+use convert_case::{Case, Casing};
+use reqwest::Url;
+use std::{error::Error, path::Path};
 
-static PROP_KEYS: [&str; 9] = [
+use super::datatype::DataType;
+
+static PROP_KEYS: [&str; 10] = [
     "type", "format", "enum", "minimum", "maximum", "minItems", "maxItems", "title", "items",
+    "$ref",
 ];
-
-#[derive(Debug)]
-enum DataType {
-    String,
-    Integer,
-    Number,
-    Boolean,
-    Object,
-    Array,
-}
-
-impl Display for DataType {
-    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        match self {
-            DataType::String => write!(f, "string"),
-            DataType::Integer => write!(f, "integer"),
-            DataType::Number => write!(f, "number"),
-            DataType::Boolean => write!(f, "boolean"),
-            DataType::Object => write!(f, "object"),
-            DataType::Array => write!(f, "array"),
-        }
-    }
-}
-
-impl FromStr for DataType {
-    type Err = String;
-    fn from_str(s: &str) -> Result<Self, Self::Err> {
-        match s {
-            "string" => Ok(DataType::String),
-            "integer" => Ok(DataType::Integer),
-            "number" => Ok(DataType::Number),
-            "boolean" => Ok(DataType::Boolean),
-            "object" => Ok(DataType::Object),
-            "array" => Ok(DataType::Array),
-            _ => Err(format!("Unknown data type: {}", s)),
-        }
-    }
-}
 
 /// Parse a JSON schema into an MD-Models data model
 pub fn parse_json_schema(path: &Path) -> Result<DataModel, Box<dyn Error>> {
@@ -69,11 +35,22 @@ pub fn parse_json_schema(path: &Path) -> Result<DataModel, Box<dyn Error>> {
     let object = create_object(&schema);
     model.objects.push(object);
 
-    // Create the rest of the objects
+    // Create the rest of the objects and enums
     let definitions = schema.get("definitions").unwrap();
-    for (_, value) in definitions.as_object().unwrap() {
-        let object = create_object(value);
-        model.objects.push(object);
+    for (key, value) in definitions.as_object().unwrap() {
+        let data_type = DataType::from_object(value);
+
+        match data_type {
+            DataType::Object { properties: _ } => {
+                let object = create_object(value);
+                model.objects.push(object);
+            }
+            DataType::Enum { values } => {
+                let enumeration = create_enum(key, &values);
+                model.enums.push(enumeration);
+            }
+            _ => {}
+        }
     }
 
     Ok(model)
@@ -83,6 +60,51 @@ pub fn parse_json_schema(path: &Path) -> Result<DataModel, Box<dyn Error>> {
 fn read_json_schema(path: &Path) -> Result<serde_json::Value, serde_json::Error> {
     let content = std::fs::read_to_string(path).expect("Could not read the JSON schema file");
     serde_json::from_str(&content)
+}
+
+fn create_enum(name: &str, values: &[String]) -> Enumeration {
+    // Create a generic mapping for the enum
+    let mappings = values
+        .iter()
+        .map(|v| (create_enum_alias(v), v.to_string()))
+        .collect();
+
+    Enumeration {
+        name: name.to_string(),
+        mappings,
+    }
+}
+
+fn create_enum_alias(name: &str) -> String {
+    // If it is a URL, get the last part of the URL and part before the .org/.com
+    let name = if let Ok(url) = Url::parse(name) {
+        url_to_enum_alias(url)
+    } else {
+        remove_special_characters(name)
+    };
+
+    name.to_case(Case::Snake).to_uppercase()
+}
+
+fn remove_special_characters(input: &str) -> String {
+    input.chars().filter(|c| c.is_alphanumeric()).collect()
+}
+
+fn url_to_enum_alias(url: Url) -> String {
+    // Get the host and path
+    let host = url.host_str().unwrap_or("");
+    let path = url.path();
+
+    // Remove the 'www.' prefix from the host if present
+    let host = host.strip_prefix("www.").unwrap_or(host);
+
+    // Replace dot and slash with underscore
+    let mut result = host.replace('.', "_");
+    result.push('_');
+    result.push_str(&path.replace('/', "_"));
+
+    // Trim trailing underscore
+    result.trim_end_matches('_').to_string()
 }
 
 /// Extract properties from a JSON schema
@@ -101,17 +123,13 @@ fn create_object(schema: &serde_json::Value) -> Object {
     let mut object = Object::new(name.to_string(), None);
 
     for (key, value) in properties {
-        let data_type = value
-            .get("type")
-            .expect("Could not find type in the property")
-            .as_str()
-            .expect("Type is not a string")
-            .parse::<DataType>()
-            .expect("Could not parse data type");
+        let data_type = DataType::from_object(value);
 
         let mut attribute = match data_type {
-            DataType::Object => process_object(key, value),
+            DataType::Object { properties } => process_object(key, &properties),
             DataType::Array => process_array(key, value),
+            DataType::Enum { values: _ } => process_enum(key),
+            DataType::Reference { reference } => process_reference(key, reference),
             _ => process_primitive(key, value),
         };
 
@@ -139,30 +157,16 @@ fn process_array(name: &str, value: &serde_json::Value) -> Attribute {
     // Get the items
     let items = value
         .get("items")
-        .expect("Could not find items in the array")
-        .as_object()
-        .expect("Items is not an object");
+        .expect("Could not find items in the array");
 
     // Check whether the items is a ref or any other type
-    let data_type = if let Some(ref_name) = items.get("$ref") {
-        ref_name
-            .as_str()
-            .expect("Ref is not a string")
-            .split('/')
-            .last()
-            .expect("Could not get the last part of the ref")
-            .to_string()
-    } else {
-        items
-            .get("type")
-            .expect("Could not find type in the array items")
-            .as_str()
-            .expect("Could not parse data type")
-            .to_string()
-    };
+    let data_type = DataType::from_object(items);
 
     // Set the data type
-    attribute.dtypes = vec![data_type];
+    attribute.dtypes = match data_type {
+        DataType::Reference { reference } => vec![reference],
+        _ => vec![data_type.to_string()],
+    };
 
     attribute
 }
@@ -181,6 +185,46 @@ fn process_primitive(name: &str, value: &serde_json::Value) -> Attribute {
     attribute
 }
 
+fn process_reference(name: &str, reference: String) -> Attribute {
+    let mut attribute = Attribute::new(name.to_string(), false);
+    attribute.dtypes = vec![reference];
+    attribute
+}
+
 fn process_object(_name: &str, _value: &serde_json::Value) -> Attribute {
     panic!("Nested object type is not supported yet");
+}
+
+fn process_enum(_name: &str) -> Attribute {
+    panic!("Property enums are currently only allowed as reference");
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_parse_json_schema() {
+        // Arrange
+        let path = Path::new("tests/data/expected_json_schema.json");
+
+        // Act
+        let model = parse_json_schema(path).unwrap();
+
+        // Assert
+        assert_eq!(model.objects.len(), 2);
+        assert_eq!(model.enums.len(), 1);
+
+        let object = &model.objects[0];
+        assert_eq!(object.name, "Test");
+        assert_eq!(object.attributes.len(), 4);
+
+        let object = &model.objects[1];
+        assert_eq!(object.name, "Test2");
+        assert_eq!(object.attributes.len(), 2);
+
+        let enumeration = &model.enums[0];
+        assert_eq!(enumeration.name, "Ontology");
+        assert_eq!(enumeration.mappings.len(), 3);
+    }
 }
