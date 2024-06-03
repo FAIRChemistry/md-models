466a4c64c5c16c9b0b15eb297b8a8748f62fc656
Jan Range <range.jan@web.de>
2024-06-03 15:35:45 +0200

    serialize enum into definitions

--- src/schema.rs
+++ src/schema.rs
@@ -6,9 +6,35 @@ use crate::primitives::PrimitiveTypes;
 use serde::{Deserialize, Serialize};
 use serde_json::json;
 use std::collections::HashSet;
+use std::fmt::Display;
+use std::hash::Hash;
 
 static DEFINITIONS_KEY: &str = "definitions";
 static SCHEMA_VERSION: &str = "http://json-schema.org/draft-07/schema";
+
+#[derive(PartialEq, Eq, Debug)]
+enum RefType {
+    Object(String),
+    Enum(String),
+}
+
+impl Hash for RefType {
+    fn hash<H: std::hash::Hasher>(&self, state: &mut H) {
+        match self {
+            RefType::Object(name) => name.hash(state),
+            RefType::Enum(name) => name.hash(state),
+        }
+    }
+}
+
+impl Display for RefType {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            RefType::Object(name) => write!(f, "{}", name),
+            RefType::Enum(name) => write!(f, "{}", name),
+        }
+    }
+}
 
 #[derive(Serialize, Deserialize)]
 struct JSONSchema {
@@ -31,11 +57,22 @@ pub fn to_json_schema(name: &String, model: &DataModel) -> String {
     let obj = objects.iter().find(|o| o.name == *name).unwrap();
     let (mut schema, used_refs) = process_class(obj, model);
 
-    for reference in used_refs {
-        let sub_obj = objects.iter().find(|o| o.name == reference).unwrap();
-        let (properties, _) = process_class(sub_obj, model);
+    // Get unique used_refs
+    let used_refs = used_refs.into_iter().collect::<HashSet<RefType>>();
 
-        schema[DEFINITIONS_KEY][reference] = properties;
+    for reference in used_refs {
+        match reference {
+            RefType::Object(name) => {
+                let sub_obj = objects.iter().find(|o| o.name == name).unwrap();
+                let (properties, _) = process_class(sub_obj, model);
+                schema[DEFINITIONS_KEY][name] = properties;
+            }
+            RefType::Enum(name) => {
+                let sub_enum = model.enums.iter().find(|e| e.name == name).unwrap();
+                let properties = process_enum(sub_enum);
+                schema[DEFINITIONS_KEY][name] = properties;
+            }
+        }
     }
 
     let schema = JSONSchema {
@@ -57,7 +94,7 @@ pub fn to_json_schema(name: &String, model: &DataModel) -> String {
 fn process_class(
     object: &object::Object,
     model: &DataModel,
-) -> (serde_json::Value, HashSet<String>) {
+) -> (serde_json::Value, HashSet<RefType>) {
     // Retrieve all object and enum names
     let object_names = model
         .objects
@@ -95,10 +132,14 @@ fn process_class(
 
         for reference in references {
             if enum_names.contains(&reference) {
-                let enumeration = model.enums.iter().find(|e| e.name == reference).unwrap();
-                process_enum_reference(&attribute.name, &mut schema["properties"], enumeration);
+                all_refs.insert(RefType::Enum(reference.clone()));
+                process_enum_reference(
+                    &attribute.name,
+                    &mut schema["properties"],
+                    reference.as_str(),
+                );
             } else if object_names.contains(&reference) {
-                all_refs.insert(reference.clone());
+                all_refs.insert(RefType::Object(reference.clone()));
                 process_reference(&mut schema["properties"], attribute, &reference);
             } else {
                 panic!("Reference {} not found in the markdown file", reference);
@@ -107,6 +148,20 @@ fn process_class(
     }
 
     (schema, all_refs)
+}
+
+fn process_enum(enumeration: &Enumeration) -> serde_json::Value {
+    let values = enumeration
+        .mappings
+        .values()
+        .cloned()
+        .collect::<Vec<String>>();
+
+    json!({
+        "title": enumeration.name,
+        "type": "string",
+        "enum": values,
+    })
 }
 
 /// Extracts primitive types and references from a list of data types.
@@ -243,18 +298,10 @@ fn process_reference(
 /// * `name` - The name of the attribute.
 /// * `properties` - The properties JSON object.
 /// * `enumeration` - The enumeration object.
-fn process_enum_reference(
-    name: &String,
-    properties: &mut serde_json::Value,
-    enumeration: &Enumeration,
-) {
+fn process_enum_reference(name: &String, properties: &mut serde_json::Value, reference: &str) {
     properties[name] = create_property(name);
-    properties[name]["type"] = json!("string");
-    properties[name]["enum"] = json!(enumeration
-        .mappings
-        .values()
-        .map(|v| v.to_string())
-        .collect::<Vec<String>>());
+    let def_path = format!("#/{}/{}", DEFINITIONS_KEY, reference);
+    properties[name]["$ref"] = json!(def_path);
 }
 
 /// Sets the data type of a reference attribute.
