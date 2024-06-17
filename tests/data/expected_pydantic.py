f500097a59c6df92d0ac3ca434ea5e2351357e7c
Jan Range <range.jan@web.de>
2024-06-17 15:47:44 +0200

    update pydantic template

--- tests/data/expected_pydantic.py
+++ tests/data/expected_pydantic.py
@@ -2,13 +2,77 @@
 
 from __future__ import annotations
 from pydantic import BaseModel, Field, ConfigDict
-from typing import Optional
+from typing import Optional, Generic, TypeVar
 from enum import Enum
 from uuid import uuid4
 from datetime import date, datetime
 
+# Filter Wrapper definition used to filter a list of objects
+# based on their attributes
+Cls = TypeVar("Cls")
+
+class FilterWrapper(Generic[Cls]):
+    """Wrapper class to filter a list of objects based on their attributes"""
+
+    def __init__(self, collection: list[Cls], **kwargs):
+        self.collection = collection
+        self.kwargs = kwargs
+
+    def filter(self) -> list[Cls]:
+        for key, value in self.kwargs.items():
+            self.collection = [
+                item for item in self.collection if self._fetch_attr(key, item) == value
+            ]
+        return self.collection
+
+    def _fetch_attr(self, name: str, item: Cls):
+        try:
+            return getattr(item, name)
+        except AttributeError:
+            raise AttributeError(f"{item} does not have attribute {name}")
+
+
+# JSON-LD Helper Functions
+def add_namespace(obj, prefix: str | None, iri: str | None):
+    """Adds a namespace to the JSON-LD context
+
+    Args:
+        prefix (str): The prefix to add
+        iri (str): The IRI to add
+    """
+    if prefix is None and iri is None:
+        return
+    elif prefix and iri is None:
+        raise ValueError("If prefix is provided, iri must also be provided")
+    elif iri and prefix is None:
+        raise ValueError("If iri is provided, prefix must also be provided")
+
+    obj.ld_context[prefix] = iri # type: ignore
+
+def validate_prefix(term: str | dict, prefix: str):
+    """Validates that a term is prefixed with a given prefix
+
+    Args:
+        term (str): The term to validate
+        prefix (str): The prefix to validate against
+
+    Returns:
+        bool: True if the term is prefixed with the prefix, False otherwise
+    """
+
+    if isinstance(term, dict) and not term["@id"].startswith(prefix + ":"):
+        raise ValueError(f"Term {term} is not prefixed with {prefix}")
+    elif isinstance(term, str) and not term.startswith(prefix + ":"):
+        raise ValueError(f"Term {term} is not prefixed with {prefix}")
+
+# Model Definitions
 
 class Test(BaseModel):
+
+    model_config: ConfigDict = ConfigDict( # type: ignore
+        validate_assigment = True,
+    ) # type: ignore
+
     name: str
     number: float = 1.0
     test2: list[Test2] = Field(default_factory=list)
@@ -39,6 +103,85 @@ class Test(BaseModel):
         }
     )
 
+    def filter_test2(self, **kwargs) -> list[Test2]:
+        """Filters the test2 attribute based on the given kwargs
+
+        Args:
+            **kwargs: The attributes to filter by.
+
+        Returns:
+            list[Test2]: The filtered list of Test2 objects
+        """
+
+        return FilterWrapper[Test2](self.test2, **kwargs).filter()
+
+
+    def set_attr_term(
+        self,
+        attr: str,
+        term: str | dict,
+        prefix: str | None = None,
+        iri: str | None = None
+    ):
+        """Sets the term for a given attribute in the JSON-LD object
+
+        Example:
+            # Using an IRI term
+            >> obj.set_attr_term("name", "http://schema.org/givenName")
+
+            # Using a prefix and term
+            >> obj.set_attr_term("name", "schema:givenName", "schema", "http://schema.org")
+
+            # Usinng a dictionary term
+            >> obj.set_attr_term("name", {"@id": "http://schema.org/givenName", "@type": "@id"})
+
+        Args:
+            attr (str): The attribute to set the term for
+            term (str | dict): The term to set for the attribute
+
+        Raises:
+            AssertionError: If the attribute is not found in the model
+        """
+
+        assert attr in self.model_fields, f"Attribute {attr} not found in {self.__class__.__name__}"
+
+        if prefix:
+            validate_prefix(term, prefix)
+
+        add_namespace(self, prefix, iri)
+        self.ld_context[attr] = term
+
+    def add_type_term(
+        self,
+        term: str,
+        prefix: str | None = None,
+        iri: str | None = None
+    ):
+        """Adds a term to the @type field of the JSON-LD object
+
+        Example:
+            # Using a term
+            >> obj.add_type_term("https://schema.org/Person")
+
+            # Using a prefixed term
+            >> obj.add_type_term("schema:Person", "schema", "https://schema.org/Person")
+
+        Args:
+            term (str): The term to add to the @type field
+            prefix (str, optional): The prefix to use for the term. Defaults to None.
+            iri (str, optional): The IRI to use for the term prefix. Defaults to None.
+
+        Raises:
+            ValueError: If prefix is provided but iri is not
+            ValueError: If iri is provided but prefix is not
+        """
+
+        if prefix:
+            validate_prefix(term, prefix)
+
+        add_namespace(self, prefix, iri)
+        self.ld_type.append(term)
+
 
     def add_to_test2(
         self,
@@ -62,6 +205,11 @@ class Test(BaseModel):
 
 
 class Test2(BaseModel):
+
+    model_config: ConfigDict = ConfigDict( # type: ignore
+        validate_assigment = True,
+    ) # type: ignore
+
     names: list[str] = Field(default_factory=list)
     number: Optional[float] = Field(default=None)
 
@@ -85,6 +233,73 @@ class Test2(BaseModel):
             "number": "schema:one",
         }
     )
+
+
+    def set_attr_term(
+        self,
+        attr: str,
+        term: str | dict,
+        prefix: str | None = None,
+        iri: str | None = None
+    ):
+        """Sets the term for a given attribute in the JSON-LD object
+
+        Example:
+            # Using an IRI term
+            >> obj.set_attr_term("name", "http://schema.org/givenName")
+
+            # Using a prefix and term
+            >> obj.set_attr_term("name", "schema:givenName", "schema", "http://schema.org")
+
+            # Usinng a dictionary term
+            >> obj.set_attr_term("name", {"@id": "http://schema.org/givenName", "@type": "@id"})
+
+        Args:
+            attr (str): The attribute to set the term for
+            term (str | dict): The term to set for the attribute
+
+        Raises:
+            AssertionError: If the attribute is not found in the model
+        """
+
+        assert attr in self.model_fields, f"Attribute {attr} not found in {self.__class__.__name__}"
+
+        if prefix:
+            validate_prefix(term, prefix)
+
+        add_namespace(self, prefix, iri)
+        self.ld_context[attr] = term
+
+    def add_type_term(
+        self,
+        term: str,
+        prefix: str | None = None,
+        iri: str | None = None
+    ):
+        """Adds a term to the @type field of the JSON-LD object
+
+        Example:
+            # Using a term
+            >> obj.add_type_term("https://schema.org/Person")
+
+            # Using a prefixed term
+            >> obj.add_type_term("schema:Person", "schema", "https://schema.org/Person")
+
+        Args:
+            term (str): The term to add to the @type field
+            prefix (str, optional): The prefix to use for the term. Defaults to None.
+            iri (str, optional): The IRI to use for the term prefix. Defaults to None.
+
+        Raises:
+            ValueError: If prefix is provided but iri is not
+            ValueError: If iri is provided but prefix is not
+        """
+
+        if prefix:
+            validate_prefix(term, prefix)
+
+        add_namespace(self, prefix, iri)
+        self.ld_type.append(term)
 
 
 class Ontology(Enum):
