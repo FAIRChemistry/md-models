a301047cc36a7b0fbbd86fc7d089b9baa03db6a1
Jan Range <range.jan@web.de>
2024-09-08 15:54:20 +0200

    fix formatting

--- src/validation.rs
+++ src/validation.rs
@@ -9,7 +9,9 @@ use colored::Colorize;
 use log::error;
 
 // Basic types that are ignored in the validation process
-const BASIC_TYPES: [&str; 7] = ["string", "number", "integer", "boolean", "float", "date", "bytes"];
+const BASIC_TYPES: [&str; 7] = [
+    "string", "number", "integer", "boolean", "float", "date", "bytes",
+];
 
 /// Validator for checking the integrity of a data model.
 pub struct Validator {
