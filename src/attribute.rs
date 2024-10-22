e875b424e02a8ded815c44174b9e7890f000b0fb
Jan Range <range.jan@web.de>
2024-10-22 18:32:45 +0200

    add python bindings

--- src/attribute.rs
+++ src/attribute.rs
@@ -1,9 +1,35 @@
+/*
+ * Copyright (c) 2024 Jan Range
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ *
+ */
+
 use crate::xmltype::XMLType;
 use serde::{de::Visitor, Deserialize, Serialize};
 use std::{error::Error, fmt, str::FromStr};
+#[cfg(feature = "python")]
+use pyo3::pyclass;
 
 /// Represents an attribute with various properties and options.
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "python", pyclass(get_all))]
 pub struct Attribute {
     /// The name of the attribute.
     pub name: String,
@@ -133,6 +159,7 @@ impl Attribute {
 
 /// Represents an option for an attribute.
 #[derive(Serialize, Deserialize, Debug, Clone, PartialEq)]
+#[cfg_attr(feature = "python", pyclass(get_all))]
 pub struct AttrOption {
     /// The key of the option.
     pub key: String,
@@ -174,6 +201,7 @@ impl AttrOption {
 }
 
 #[derive(Debug, Clone)]
+#[cfg_attr(feature = "python", pyclass)]
 pub enum DataType {
     Boolean(bool),
     Integer(i64),
