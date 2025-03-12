08b7f26c16ed4dbf861b364c43d3dd31a5d33184
Jan Range <30547301+JR-1991@users.noreply.github.com>
2025-03-12 17:49:32 +0100

    Squashed commit of the following:
    commit 8b7b3cb3aadd66dc2f30f5465df53a1008df3a8f
    Author: Jan Range <30547301+JR-1991@users.noreply.github.com>
    Date:   Wed Mar 12 17:48:39 2025 +0100
    
        remove formatted files
    
    commit ef13d21612aedb4348604e72f05db8a710818223
    Author: Jan Range <30547301+JR-1991@users.noreply.github.com>
    Date:   Wed Mar 12 17:46:56 2025 +0100
    
        fix

--- tests/data/expected_golang.go
+++ tests/data/expected_golang.go
@@ -5,11 +5,6 @@
 
 package model
 
-import (
-	"encoding/json"
-	"fmt"
-)
-
 //
 // Type definitions
 //
@@ -20,60 +15,62 @@ import (
 // incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis
 // nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
 type Test struct {
-	Name     string         `json:"name" `
-	Number   TestNumberType `json:"number,omitempty" `
-	Test2    []Test2        `json:"test2,omitempty" `
-	Ontology Ontology       `json:"ontology,omitempty" `
+    Name string `json:"name" `
+    Number TestNumberType `json:"number,omitempty" `
+    Test2 []Test2 `json:"test2,omitempty" `
+    Ontology Ontology `json:"ontology,omitempty" `
 }
 
 // Test2
 type Test2 struct {
-	Names  []string `json:"names,omitempty" `
-	Number float64  `json:"number,omitempty" `
+    Names []string `json:"names,omitempty" `
+    Number float64 `json:"number,omitempty" `
 }
 
+//
 // Enum definitions
+//
 type Ontology string
 
 const (
-	ECO Ontology = "https://www.evidenceontology.org/term/"
-	GO  Ontology = "https://amigo.geneontology.org/amigo/term/"
-	SIO Ontology = "http://semanticscience.org/resource/"
+    ECO Ontology = "https://www.evidenceontology.org/term/"
+    GO Ontology = "https://amigo.geneontology.org/amigo/term/"
+    SIO Ontology = "http://semanticscience.org/resource/"
 )
 
 // TestNumberType represents a union type that can hold any of the following types:
 // - float
 // - string
 type TestNumberType struct {
-	Float  float64
-	String string
+    Float float64
+    String string
 }
 
 // UnmarshalJSON implements custom JSON unmarshaling for TestNumberType
 func (t *TestNumberType) UnmarshalJSON(data []byte) error {
-	// Reset existing values
-	t.Float = 0
-	t.String = ""
-	var floatValue float64
-	if err := json.Unmarshal(data, &floatValue); err == nil {
-		t.Float = floatValue
-		return nil
-	}
-	var stringValue string
-	if err := json.Unmarshal(data, &stringValue); err == nil {
-		t.String = stringValue
-		return nil
-	}
-	return fmt.Errorf("TestNumberType: data is neither float, string")
+    // Reset existing values
+    t.Float = 0
+    t.String = ""
+    var floatValue float64
+    if err := json.Unmarshal(data, &floatValue); err == nil {
+        t.Float = floatValue
+        return nil
+    }
+    var stringValue string
+    if err := json.Unmarshal(data, &stringValue); err == nil {
+        t.String = stringValue
+        return nil
+    }
+    return fmt.Errorf("TestNumberType: data is neither float, string")
 }
 
 // MarshalJSON implements custom JSON marshaling for TestNumberType
 func (t TestNumberType) MarshalJSON() ([]byte, error) {
-	if t.Float != 0 {
-		return json.Marshal(t.Float)
-	}
-	if t.String != "" {
-		return json.Marshal(t.String)
-	}
-	return []byte("null"), nil
+    if t.Float != 0 {
+        return json.Marshal(t.Float)
+    }
+    if t.String != "" {
+        return json.Marshal(t.String)
+    }
+    return []byte("null"), nil
 }
